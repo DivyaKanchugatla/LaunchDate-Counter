d1c08cd3442c90bab53c3884b3419b58dda911a5
DIVYA KANCHUGATLA <divyakanchugatla5@gmail.com>
2023-04-22 13:49:46 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Counter from './components/Countdown';
 
 const App = () => {
-  const launchDate = new Date('2023-04-21T18:30:00Z');
+  const launchDate = new Date('2023-04-27T18:30:00Z');
 
   return (
     <div>
